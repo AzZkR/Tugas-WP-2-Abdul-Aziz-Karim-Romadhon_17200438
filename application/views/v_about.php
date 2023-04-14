7bce4026cbd6e6eb34ae06c3cdd126ffbc5765da
AzZkR <azzkr22@gmail.com>
2023-04-15 00:48:11 +0700

    tugaswp2

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Aziz</li>
+        <li>Nama Belakang : Romadhon</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> Jalan Drs.Sharjo 3 Rt 033 Rw 0122 Kel. Kemayaronan Kec. Kemayoran </li>
+</ul>
+
+<h4>Tempat Tanggal Lahir</h4>
+<ul type="none">
+    <li>Jakarta, 25 Desember 1920</li>
+</ul>
+ <h4>Hobi</h4>
+ <ul type="square">
+    <li>Tidur Tapi Pengen Dapet Duit</li>
+</ul>
+</section>
